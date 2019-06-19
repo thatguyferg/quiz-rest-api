d28497aac53ff915455c3ddf44172d1eefc161a8
thatguyferg <austen.a.ferguson@gmail.com>
2019-06-19 17:29:48 -0400

    Condensed blueprint config into resources/__init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -5,7 +5,7 @@ def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_filename)
 
-    from project.routes import api_bp
+    from .resources import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
     from project.model import db
